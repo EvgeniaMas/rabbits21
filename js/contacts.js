3ec60a0c5a27abda284c0426cd631d25bf713a81
Евгения Масликова < >
2019-05-17 10:52:00 +0300

    contacts

---
+++ js/contacts.js
@@ -0,0 +1,65 @@
+//Маска для телефона
+$(function() {
+    $("input[type='tel']").mask('+7(999) 999-99-99');   
+});
+
+$(document).on('click', 'input[type="tel"]', function(){
+$(this).focus();
+});
+
+$("input[type='email']").on('blur', function () {
+  let email = $(this).val();
+  
+  if (email.length > 0
+  && (email.match(/.+?\@.+/g) || []).length !== 1) {        
+  }
+
+});
+
+$(document).ready(function() { 
+    $("#contacts_form").submit(function(){ 
+        var form = $(this); 
+        var error = false; 
+        form.find('input.required_data').each( function(){ 
+            if ($(this).val() == '') { 
+                form.find('.error').text('Зaпoлнитe поля!');
+                error = true; 
+            }
+        });
+        if (!error) { 
+            var data = form.serialize(); 
+            $.ajax({ 
+                type: 'POST', 
+                url: 'call.php', 
+                dataType: 'json', 
+                data: data,
+                beforeSend: function(data) { 
+                    form.find('button[type="submit"]').attr('disabled', 'disabled'); 
+                },
+                success: function(data){ 
+                     if (data['error']) { 
+                         alert(data['error']);
+                     } 
+
+                    else {                                     
+                     $("input").each(function () {
+                       $(this).data("defvalue", this.value);
+                    });                                     
+                  $("#contacts_form").css("display", "none");
+                  $("#sent_message").css("display", "block");                                  
+                           
+                    }
+                },
+                error: function (xhr, ajaxOptions, thrownError) { 
+                    alert(xhr.status); 
+                    alert(thrownError);
+                },
+                complete: function(data) {
+                    form.find('button[type="submit"]').prop('disabled', false); 
+                }
+                          
+                 });
+        }
+        return false;
+    });
+});
